60cc4edaf006824200c597d59354ef5b8fddc3eb
Colin Jaffe <balloonasaurus@gmail.com>
2021-02-23 11:39:12 -0500

    Moves getInput to its own file.

--- mental-age.js
+++ mental-age.js
@@ -1,12 +1,1 @@
-console.log("Welcome to the node-along!");
-
-// DON'T TOUCH THE BELOW HELPER FUNCTION
-// It's just a nice function so you don't
-// have to deal with arrays yet.
-
-// If you call the below function in your
-// code, you'll get back the first thing
-// the user typed after `node [filename]`.
-function getInput() {
-  return process.argv[2];
-}
+const getInput = require("./get-input.js");
